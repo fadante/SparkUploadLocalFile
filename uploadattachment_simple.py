20bc7ad2132b1b39c0231698799094dabca2f6ac
Taylor Hanson <tahanson@cisco.com>
2016-07-20 15:46:49 -0400

    added sample script

---
+++ uploadattachment_simple.py
@@ -0,0 +1,18 @@
+from requests_toolbelt import MultipartEncoder
+import requests
+
+filepath    = '/Users/taylorhanson/Desktop/screenshot.png'
+filetype    = 'image/png'
+roomId      = 'SOME ROOM'
+token       = 'YOUR ACCOUNT BEARER TOKEN'
+url         = "https://api.ciscospark.com/v1/messages"
+
+my_fields={'roomId': roomId, 
+           'text': 'Hello World',
+           'files': ('screenshot', open(filepath, 'rb'), filetype)
+           }
+m = MultipartEncoder(fields=my_fields)
+r = requests.post(url, data=m,
+                  headers={'Content-Type': m.content_type,
+                           'Authorization': 'Bearer ' + token})
+print r.json()
